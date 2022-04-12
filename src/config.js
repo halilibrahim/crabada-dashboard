a00606513785cd59473c6648acf535dcacc42675
Halil İbrahim Karaalp <halilibrahim@karaalp.org>
2022-04-12 22:50:16 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,19 +1,19 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
+    "collection_address": "0x1b7966315eF0259de890F38f1bDB95Acc03caCdD",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "137",
+    "block_chain_id": "43114",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "My NFT Collection",
+    "title": "Crabada NFT Collection",
 
     // 4. (Optional) Display the floor price chart
     "timeseries_chart": true,
 
     // 5. (Optional) Set your banner image
-    "banner_picture": "https://www.superflexfitness.com/wp-content/uploads/2017/03/3D-banner-background.jpg",
+    "banner_picture": "https://287599335-files.gitbook.io/~/files/v0/b/gitbook-legacy-files/o/assets%2F-MhsCcn6BcNJkR-dyu4p%2F-MiD1cvq7nd08ce7OCpA%2F-MiD2XUTOSS0Qc3BLN1b%2FCRA-Token.png?alt=media&token=1ef4a8cf-0dcf-44b7-85c5-79991ca828a9",
   },
 
   // Supported networks - just for your reference
